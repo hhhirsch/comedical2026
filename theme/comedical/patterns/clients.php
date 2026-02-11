3a5453dc2a6e00388396ea2f276a0400cd3a9f5f
hhhirsch <hirsch.hans92@gmail.com>
2026-02-11 21:26:15 +0100

    Add clients logo strip pattern below hero

---
+++ theme/comedical/patterns/clients.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Comedical Clients
+ * Slug: comedical/clients
+ * Categories: featured
+ * Inserter: yes
+ */
+?>
+<!-- wp:group {"tagName":"section","className":"cm-clients","layout":{"type":"constrained","wideSize":"1200px"}} -->
+<section class="wp-block-group cm-clients">
+  <!-- wp:group {"className":"cm-container cm-clients__inner","layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"center","flexWrap":"nowrap"}} -->
+  <div class="wp-block-group cm-container cm-clients__inner">
+    <!-- wp:paragraph {"className":"cm-clients__label"} -->
+    <p class="cm-clients__label">Kunden</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:group {"className":"cm-clients__list","layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"center","flexWrap":"nowrap"}} -->
+    <div class="wp-block-group cm-clients__list">
+      <!-- wp:paragraph {"className":"cm-clients__item"} -->
+      <p class="cm-clients__item">Logo 1</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"className":"cm-clients__item"} -->
+      <p class="cm-clients__item">Logo 2</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"className":"cm-clients__item"} -->
+      <p class="cm-clients__item">Logo 3</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"className":"cm-clients__item"} -->
+      <p class="cm-clients__item">Logo 4</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"className":"cm-clients__item"} -->
+      <p class="cm-clients__item">Logo 5</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</section>
+<!-- /wp:group -->
